4087a190d55b492fedbc291b5e59c7cef10759c4
annejones817 <annejones817@gmail.com>
2016-10-07 13:33:42 -0700

    Adding additional API calls to get more data and higher res images. Re-wrote html generation to rely on new data.

--- app.js
+++ app.js
@@ -1,53 +1,56 @@
 $(document).ready(function(event){
+		var _app_id = '863760f1'; 
+		var _app_key = 'a00fc0ae6e3fc72da3b4030555e967a8';
+		var initialResults = [];
+		var recipeDetails = [];
+
 	function getRecipeData (userSearch, callback){
 		var baseURL = 'https://api.yummly.com/v1/api/recipes';
 		var query = {
 			q: userSearch, 
-			_app_id: '863760f1', 
-			_app_key: 'a00fc0ae6e3fc72da3b4030555e967a8'
+			_app_id: _app_id, 
+			_app_key: _app_key
 		};
-		$.getJSON(baseURL, query, callback);
+		$.ajax({
+		  url: baseURL,
+		  data: query
+		}).done(function(results) {
+		  results.matches.forEach(function(element){
+			baseURL = 'http://api.yummly.com/v1/api/recipe/' + element.id;
+			query = {
+				_app_id: _app_id, 
+				_app_key: _app_key
+				}; 
+			$.getJSON(baseURL, query, callback);
+		  })
+		});
 	}
 
 	function generateResultsHTML(data) {
-		if (data.matches.length>0) {
-		var resultsHTML = '<div class="row">';
-		for (var i=0; i<data.matches.length; i++) {
-			if (i>0 && i%2===0) {
-				resultsHTML += '<div class="row">';
-			}
-			resultsHTML += 
-				'<div class="col-6">' +
+		var counter = 0;
+		var resultsHTML = '';
+		if(counter>0 & counter%2===0) {resultsHTML += '<div class="row">'};
+		resultsHTML += '<div class="col-6">' +
 		            '<div class="result-card">' +
 		                 '<div class="name-source">' +   
-		                    '<a class="name-link" href="https://www.yummly.com/recipe/' + data.matches[i].id + '"target="_blank">' +
-		                    '<h2 class="name">'+ data.matches[i].recipeName +'</h2></a>'+
-		                    '<h3 class="source">By: '+ data.matches[i].sourceDisplayName +'</h3>' +
+		                    '<a class="name-link" href="https://www.yummly.com/recipe/' + data.id + '"target="_blank">' +
+		                    '<h2 class="name">'+ data.name +'</h2></a>'+
+		                    '<h3 class="source">By: '+ data.source.sourceDisplayName +'</h3>' +
 		                 '</div>' +   
 		                '<div class="recipe-image">' +
-		                    '<a href="https://www.yummly.com/recipe/' + data.matches[i].id + '"target="_blank">' +
-		                        '<img alt="recipe-image" class="recipe-image" src="'+ data.matches[i].imageUrlsBySize['90'] +'"></a>' +
+		                    '<a href="https://www.yummly.com/recipe/' + data.id + '"target="_blank">' +
+		                        '<img alt="recipe-image" class="recipe-image" src="'+ data.images[0].imageUrlsBySize['360'] +'"></a>' +
 		                '</div>' +    
 		                '<div class="rating-ingredients">' +
-			                '<p class="rating">Rating: ' + data.matches[i].rating + '</p>' +   
-			                '<p class="ingredients">Ingredients: ' + data.matches[i].ingredients.join(', ') + '</p>' +
+			                '<p class="rating">Rating: ' + data.rating + '</p>' +   
+			                '<p class="ingredients">Ingredients: ' + data.ingredientLines.join(', ') + '</p>' +
 			            '</div>' +    
 		            '</div>' +
 		        '</div>' ;
-			if ((i+1)%2===0) {
-				resultsHTML += '</div>';
-			}
-		}
-		if (data.matches.length%3 !== 0) {
-			resultsHTML+= '</div>';
-		}
-		resultsHTML += data.attribution.html;
+		if(counter>0 & (counter+1)%2===0) {resultsHTML += '</div>'};
+		counter += 1;
+		console.log(resultsHTML);
 		$('main').append(resultsHTML);
-	} else {
-		$('main').append(
-			"<div class='row'><div class='col-12'><div class='no-results'>Sorry, there are no results for your search.</div></div></div>"	
-			);
-	}
 	}
 			
 
@@ -65,7 +68,6 @@ $(document).ready(function(event){
 	});
 
 	$('.header-contents').on('click', '.search-again', function(event){
-		console.log('detected');
 		location.reload(true);
 	});
 
